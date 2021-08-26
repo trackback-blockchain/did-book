4b875d2bf99970d8d6f19c22f1b60d74846cc7ab
Gayan <gayan@track-back.co>
2021-08-27 09:44:57 +1200

    Terraform setup

--- terraform/ap-southeast-2/backend.tf
+++ terraform/ap-southeast-2/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "trackback-terraform"
     dynamodb_table = "terraform-locks"
-    key            = "trackback-issuer-demo.tfstate"
+    key            = "trackback-did-book.tfstate"
     region         = "ap-southeast-2"
     encrypt        = true
     acl            = "bucket-owner-full-control"
